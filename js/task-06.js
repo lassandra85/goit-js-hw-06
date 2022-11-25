540129b5a4046238ab052eff400381d433da9915
lassandra85 <lassandra85@gmail.com>
2022-11-25 19:29:51 +0100

    add

--- js/task-06.js
+++ js/task-06.js
@@ -4,9 +4,13 @@ const validationInput = document.querySelector("#validation-input");
 const validation = (event) => {
     
     if (event.currentTarget.value.length === Number(validationInput.dataset.length)) {
+        validationInput.classList.remove("invalid");
         validationInput.classList.add("valid");
+
     } else {
+        validationInput.classList.remove("valid");
         validationInput.classList.add("invalid");
+
     }
 }
 
