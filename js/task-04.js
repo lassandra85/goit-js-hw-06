d109a36649b77185ebd0546650004b50ee4a83de
lassandra85 <lassandra85@gmail.com>
2023-12-06 23:46:11 +0100

    tasc-4

--- js/task-04.js
+++ js/task-04.js
@@ -17,3 +17,14 @@ const handleIncrement = () => {
 btnDecrement.addEventListener("click", handleDecrement);
 btnIncrement.addEventListener("click", handleIncrement);
 
+
+/* 
+const counterValue = document.querySelector('#value');
+counterValue.textContent = 0;
+
+const buttonDecrement = document.querySelector('button[data-action="decrement"]');
+const onClickbuttonDecrement = buttonDecrement.addEventListener('click', () => {counterValue.textContent = Number(counterValue.textContent) - 1} );
+
+const buttonIncrement = document.querySelector('button[data-action="increment"]');
+const onClickbuttonIncrement = buttonIncrement.addEventListener('click', () => {counterValue.textContent = Number(counterValue.textContent) + 1} );
+ */
