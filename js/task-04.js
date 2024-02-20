f1a4212e0a4dbbdef51a894fdb4470f425310c8f
lassandra85 <lassandra85@gmail.com>
2024-02-21 00:24:46 +0100

    task-4

--- js/task-04.js
+++ js/task-04.js
@@ -1,30 +1,16 @@
-const btnDecrement = document.querySelector('[data-action="decrement"]');
-const btnIncrement = document.querySelector('[data-action="increment"]');
-const counter = document.querySelector("#value");
-let counterValue = 0;
-
-const handleDecrement = () => {
-    counterValue -= 1;
-    counter.textContent = counterValue;
-}
-
-const handleIncrement = () => {
-    counterValue += 1;
-    counter.textContent = counterValue;
-}
-
-
-btnDecrement.addEventListener("click", handleDecrement);
-btnIncrement.addEventListener("click", handleIncrement);
-
-
-/* 
-const counterValue = document.querySelector('#value');
+const counterValue = document.querySelector("#value");
 counterValue.textContent = 0;
 
-const buttonDecrement = document.querySelector('button[data-action="decrement"]');
-const onClickbuttonDecrement = buttonDecrement.addEventListener('click', () => {counterValue.textContent = Number(counterValue.textContent) - 1} );
+const buttonDecrement = document.querySelector(
+  'button[data-action="decrement"]'
+);
+const onClickbuttonDecrement = buttonDecrement.addEventListener("click", () => {
+  counterValue.textContent = Number(counterValue.textContent) - 1;
+});
 
-const buttonIncrement = document.querySelector('button[data-action="increment"]');
-const onClickbuttonIncrement = buttonIncrement.addEventListener('click', () => {counterValue.textContent = Number(counterValue.textContent) + 1} );
- */
+const buttonIncrement = document.querySelector(
+  'button[data-action="increment"]'
+);
+const onClickbuttonIncrement = buttonIncrement.addEventListener("click", () => {
+  counterValue.textContent = Number(counterValue.textContent) + 1;
+});
