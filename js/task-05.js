136f6cb19ea6f526d055cb06c90e5b161b115d88
lassandra85 <lassandra85@gmail.com>
2024-02-21 23:12:24 +0100

    task-5

--- js/task-05.js
+++ js/task-05.js
@@ -2,14 +2,11 @@ const nameInput = document.querySelector("#name-input");
 const nameOutput = document.querySelector("#name-output");
 
 const guest = ({ currentTarget }) => {
-    
-    if (currentTarget.value === '') {
-        nameOutput.textContent = 'Anonymous';
-    } else {
-        nameOutput.textContent = currentTarget.value;
-    }
+  if (currentTarget.value === "") {
+    nameOutput.textContent = "Anonymous";
+  } else {
+    nameOutput.textContent = currentTarget.value;
+  }
 };
 
 nameInput.addEventListener("input", guest);
-
-
